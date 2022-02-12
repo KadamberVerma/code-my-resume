b40bd2606b0ca38ecd1489f8527055a00d127442
KadamberVerma <kadamberverma@gmail.com>
2022-02-12 18:32:03 +0545

    create Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Sign in</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Resume Builder</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Resume Samples</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">How to Write a Resume</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
