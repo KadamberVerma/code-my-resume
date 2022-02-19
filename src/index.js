77533f45ff3066bf2cac91eaa2640cd30e788939
KadamberVerma <kadamberverma@gmail.com>
2022-02-19 16:16:41 +0545

    Make Footer stick to the bottom

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDom from 'react-dom';
 import './bootstrap.min.css';
 import App from './components/App';
+import './styles.css';
 
 ReactDom.render(<App />, document.querySelector('#root'));
